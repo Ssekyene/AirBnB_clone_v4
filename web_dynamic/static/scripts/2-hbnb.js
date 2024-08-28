8deaac8fa4e1a83973d18243eca47f84553ac26d
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-28 14:12:21 +0000

    mmodified 2-hbnb script

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,28 +1,31 @@
-$("document").ready(function () {
-  const url = "http://" + window.location.hostname + ":5001/api/v1/status/";
+$('document').ready(function () {
+  // toggles the top right circle color for api presence or absence
+  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
   $.ajax({
     url: url,
-    method: "GET",
-    success: function(response) {
-      if (response.status === "OK") {
-        $("DIV#api_status").addClass("available");
-      } else {
-        $("DIV#api_status").removeClass("available");
+    method: 'GET',
+    success: function (response) {
+      if (response.status === 'OK') {
+        $('DIV#api_status').addClass('available');
       }
+    },
+    error: () => {
+      $('DIV#api_status').removeClass('available');
     }
   });
 
-  let amenities = {};
+  // displays checked amenities in the amenities filter section
+  const amenities = {};
   $('INPUT[type="checkbox"]').change(function () {
-    if ($(this).is(":checked")) {
-      amenities[$(this).attr("data-id")] = $(this).attr("data-name");
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      delete amenities[$(this).attr("data-id")];
+      delete amenities[$(this).attr('data-id')];
     }
     if (Object.values(amenities).length === 0) {
-      $(".amenities H4").html("&nbsp;");
+      $('.filter_amenities H4').html('&nbsp;');
     } else {
-      $(".amenities H4").text(Object.values(amenities).join(", "));
+      $('.filter_amenities H4').text(Object.values(amenities).join(', '));
     }
   });
 });
